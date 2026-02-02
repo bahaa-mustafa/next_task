41091485ad317d41b919aed3b5050eecef85e45a
bahaa-mustafa <bahaamustafabadr@gmail.com>
2026-02-02 12:58:39 +0200

    gererate custom hook for search by debounce after 3 second as default

--- components/filtterTypes.jsx
+++ components/filtterTypes.jsx
@@ -4,7 +4,7 @@ function FilterTypes({assetsType, selectedAssetType, setSelectedAssetType}) {
   return (
     <select name="" id="" className="border border-gray-300 rounded-md px-4 py-2" value={selectedAssetType} onChange={(e) => setSelectedAssetType(e.target.value)}>
       {assetsType.map((assetType) => (
-        <option key={assetType.id} value={assetType.name}>{assetType.name}</option> 
+        <option key={assetType.id} value={assetType.type}>{assetType.type}</option> 
       ))}
     </select>
   )
